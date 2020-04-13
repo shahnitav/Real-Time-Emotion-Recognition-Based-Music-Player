94ad95462a0430ed3b288b1661a1fe3f64d850c5
AmishaMe24 <amishamehta24@gmail.com>
2020-04-14 02:54:55 +0530

    Vlc Media Player

--- Tensorflow/emotions.py
+++ Tensorflow/emotions.py
@@ -9,6 +9,9 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
+import vlc
+import time
+from random import randint
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # command line argument
@@ -40,6 +43,16 @@ def plot_model_history(model_history):
     axs[1].legend(['train', 'val'], loc='best')
     fig.savefig('plot.png')
     plt.show()
+
+#music player function
+def music_player(emotion_str):
+    i = str(randint(1,6))
+    path = "D:\Amisha\Real-Time-Emotion-Recognition-Based-Music-Player\Tensorflow\songs"
+    player = vlc.MediaPlayer(path + "/" + emotion_str +"\song"+ i + ".mp3")
+    player.play()
+    time.sleep(20)
+    player.stop()
+
 
 # Define data generators
 train_dir = 'data/train'
@@ -110,30 +123,43 @@ elif mode == "display":
     # dictionary which assigns each label an emotion (alphabetical order)
     emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
-    # start the webcam feed
-    cap = cv2.VideoCapture(0)
-    while True:
-        # Find haar cascade to draw bounding box around face
-        ret, frame = cap.read()
-        if not ret:
-            break
-        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+    #File to append the emotions
+    path = "D:\Amisha\Real-Time-Emotion-Recognition-Based-Music-Player\Tensorflow"
+    with open(path+"\emotion.txt","w") as emotion_file:
+        #music player setup 
+        #player = vlc.MediaPlayer("file:///path/to/track.mp3")
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
-            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-            if maxindex == 6:
-                playsound('audio.mp3')
+        # start the webcam feed
+        cap = cv2.VideoCapture(0)
+        while True:
+            # Find haar cascade to draw bounding box around face
+            ret, frame = cap.read()
+            if not ret:
+                break
+            facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
 
-        cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+                roi_gray = gray[y:y + h, x:x + w]
+                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+                prediction = model.predict(cropped_img)
+                maxindex = int(np.argmax(prediction))
+                cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                if maxindex == 3:
+                    #player = vlc.MediaPlayer("D:\Amisha\Real-Time-Emotion-Recognition-Based-Music-Player\Tensorflow\songs\Happy\song1.mp3")
+                    #player.play()
+                    #time.sleep(20)
+                    #player.stop()
+                    music_player(emotion_dict[maxindex])
+                emotion_file.write(emotion_dict[maxindex]+"\n")
+                emotion_file.flush()
+
+            cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
     cap.release()
     cv2.destroyAllWindows()
+
