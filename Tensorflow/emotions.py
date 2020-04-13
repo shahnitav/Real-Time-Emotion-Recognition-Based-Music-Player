9a7f2fd80f7e0f74529cf40d1a0b0df60f07691c
Nitav Shah <shahnitav@gmail.com>
2020-04-14 02:20:25 +0530

    update

--- Tensorflow/emotions.py
+++ Tensorflow/emotions.py
@@ -120,17 +120,25 @@ elif mode == "display":
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
 
+        myfile = open('result.txt','w')
+        myfile.close()
+        myfile = open('result.txt','a')
+
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
             roi_gray = gray[y:y + h, x:x + w]
             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
             prediction = model.predict(cropped_img)
             maxindex = int(np.argmax(prediction))
+            myfile.write('\n')
+            myfile.write('Nitav')
             cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+    myfile.close()
     cap.release()
     cv2.destroyAllWindows()
+    
